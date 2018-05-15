ec33061a80f68f49d143790a99103bb81ad67d3a
turathalanbiaa <turathalanbiaa@gmail.com>
2018-05-15 11:56:09 +0300

    modified city

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::git("/api/daleel/calender", function (){
+Route::get("/api/daleel/calender", function (){
     return \App\Model\DaleelAlsaam\Calender::all();
 });
